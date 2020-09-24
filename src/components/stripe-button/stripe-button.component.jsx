3572593e9cfa0fbc2240c48fa2b39ad0f2533df1
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2020-09-24 21:41:10 +0800

    Updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51HUtvaFHuLnKEOo3LlhkaiErx2FNrbzAoGChuvPlzbhmpVxaPZOBgGWtQVXB9Lhsba5ldTm7GHGpevoJ2R1Qjo0600WHRHc3Bo';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing 3-L17'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
